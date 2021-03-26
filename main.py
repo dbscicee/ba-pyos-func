cef441377feaa0ffb0fd6eb504fadcdb00eda4be
balaji kesavan <contactbalajik7@icloud.com>
2021-03-26 06:50:31 +0530

    list func update

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('Bala')
 
+
+# This represents the list features
+def displayList():
+ testList1 = ["Test1", "Test11", "Test111"]
+ testList2 = list(("Test2", "Test22", "Test222"))
+
+ print("The List Values are ",testList1)
+ print("The length of the list are ",len(testList1))
+ print("The type of the list are",type(testList1))
+ print("The List Values are ",testList2)
+ print("The length of the list are ",len(testList2))
+ print("The type of the list are",type(testList2))
+
+displayList()
